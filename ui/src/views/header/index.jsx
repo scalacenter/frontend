c636e3ecd158846308957058fe16a0ba952c1457
Simon Adcock <simon.adcock@guardian.co.uk>
2017-11-28 15:35:29 +0000

    dumb component

--- ui/src/views/header/index.jsx
+++ ui/src/views/header/index.jsx
@@ -1,3 +1,33 @@
 // @flow
 
-export default () => <div>Hello</div>;
+import {
+    header,
+    support,
+    topnav,
+    nav,
+    logo,
+    dropdown,
+} from './style.css';
+
+export default () => (
+    <header style={header}>
+        <div style={support}>become a supporter</div>
+        <ul style={topnav}>
+            <li>subscribe</li>
+            <li>find a job</li>
+            <li>sign in </li>
+            <li>search</li>
+        </ul>
+        <h1 style={logo}>the guardian</h1>
+        <nav>
+            <ul style={nav}>
+                <li>news</li>
+                <li>opinion</li>
+                <li>sport</li>
+                <li>arts</li>
+                <li>lifestyle</li>
+                <li style={dropdown}>sections</li>
+            </ul>
+        </nav>
+    </header>
+);
